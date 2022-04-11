0d84e47c572120e616d22a26a309beaceb6e3976
Chris Pittman <chrispittman73@gmail.com>
2022-04-11 12:19:10 -0500

    temp

--- src/Commands/AddLti1p3Platform.php
+++ src/Commands/AddLti1p3Platform.php
@@ -16,9 +16,9 @@ class AddLti1p3Platform extends Command
      * @var string
      */
     protected $signature = 'lti:add_platform_1.3
-                            {lms_type=custom : custom, canvas-cloud}
+                            {lms_type?}
                             {--client_id=}
-                            {--deployment_id=';
+                            {--deployment_id=}';
 
     /**
      * The console command description.
@@ -44,18 +44,37 @@ class AddLti1p3Platform extends Command
      */
     public function handle()
     {
+        if (!$this->argument('lms_type') || !$this->option('client_id') || !$this->option('deployment_id')) {
+            $this->warn("Usage: php artisan lti:add_platform_1.3 {lms_type} --client=id=XXX --deployment_id=YYY");
+            $this->warn("  lms_type can be one of:");
+            $this->warn("    * 'canvas-cloud' - Cloud-hosted instances of Canvas LMS");
+            $this->warn("    * 'custom' - Any other LMS.");
+            $this->warn('See https://github.com/longhornopen/laravel-celtic-lti/wiki/LTI-Connection-IDs for the locations of the client and deployment IDs in your LMS.');
+            return 0;
+        }
+
         $pdo = DB::connection()->getPdo();
         $dataConnector = LTI\DataConnector\DataConnector::getDataConnector($pdo, '', 'pdo');
+        $deployment_id = $this->option('deployment_id');
+        $client_id = $this->option('client_id');
 
         if ($this->argument('lms_type') === 'canvas-cloud') {
             PlatformCreator::createLTI1p3PlatformCanvasCloud(
                 $dataConnector,
                 $deployment_id,
                 $client_id);
-        } else if ($this->argument('lms_type') === 'custom') {
-            throw new \RuntimeException("Custom LTI 1.3 configurations not supported yet.  Use PlatformCreator directly for the moment.");
+            $this->info("Successfully created.");
+            return 0;
         }
-            throw new \RuntimeException("Unknown lms_type '".$this->argument('lms_type')."'");
+
+        if ($this->argument('lms_type') === 'custom') {
+            // FIXME hook up a bunch of command-line flags to the args in PlatformCreator::createLTI1p3Platform
+            $this->error("Custom LTI 1.3 configurations not supported yet.  Use PlatformCreator directly for the moment.");
+            $this->error("  (We're actively seeking contributions helping to support other LMSes natively.)");
+            return 1;
         }
+
+        $this->error("Unknown lms_type '".$this->argument('lms_type')."'");
+        return 1;
     }
 }
--- src/PlatformCreator.php
+++ src/PlatformCreator.php
@@ -47,14 +47,14 @@ class PlatformCreator
 
         $platform->name = $platform_id;
         $platform->authorizationServerId = $authorization_server_id;
-        $platform->jku = 'https://canvas.instructure.com/api/lti/security/jwks';
-        $platform->authenticationUrl = 'https://canvas.instructure.com/api/lti/authorize_redirect';
-        $platform->accessTokenUrl = 'https://canvas.instructure.com/login/oauth2/token';
+        $platform->jku = $jku;
+        $platform->authenticationUrl = $authentication_url;
+        $platform->accessTokenUrl = $access_token_url;
         $platform->rsaKey = $rsa_key;
         $platform->signatureMethod = $signature_method;
 
-        $platform->save();
         $platform->enabled = true;
+        $platform->save();
     }
 
     public static function createLTI1p3PlatformCanvasCloud(
@@ -63,22 +63,6 @@ class PlatformCreator
         $client_id
     )
     {
-        // FIXME: this as info help in Command
-        /*
-
-https://community.canvaslms.com/t5/Canvas-Question-Forum/LTI-1-3-Client-ID-and-deployment-id-to-differentiate-LMS-and/m-p/238813
-// for client_id and deployment_id
-https://canvas.instructure.com/doc/api/file.lti_dev_key_config.html
-
- * After you create and configure the Developer key for your app (see detailed instructions here) you can get both the client_id and the deployment_id.
-
-The client_id is the developer key; it's a numeric value that looks something like
-
-32450000000000542.
-
-The deployment_id can be found in Admin > Settings | Apps | App Configurations. It is an alphanumeric string that might look something like 10832:7db538f4ca75c02373788b2ac73869cf572b0b68
- */
-
         $platform_id = 'https://canvas.instructure.com';
         $rsa_key = null;  // a public key is not required if a JKU is available
         $signature_method = 'RS256';

ade5d80530cc6eea8e8e989963d0c2e46cac6455
Chris Pittman <chrispittman73@gmail.com>
2024-04-25 09:17:00 -0500

    cleanup

--- src/DataConnector/DataConnectorProviderFactory.php
+++ src/DataConnector/DataConnectorProviderFactory.php
@@ -3,7 +3,8 @@
 namespace LonghornOpen\LaravelCelticLTI\DataConnector;
 
 class DataConnectorProviderFactory {
-    public static function getDataConnectorProvider() {
+    public static function getDataConnectorProvider()
+    {
         $dc_class = config('lti.data_connector_provider', LaravelDataConnectorProvider::class);
         return new $dc_class();
     }
--- src/DataConnector/LaravelDataConnectorProvider.php
+++ src/DataConnector/LaravelDataConnectorProvider.php
@@ -8,14 +8,15 @@ use LonghornOpen\LaravelCelticLTI\DataConnector\DataConnectorProvider;
 
 class LaravelDataConnectorProvider implements DataConnectorProvider
 {
-    public function getDataConnector() {
+    public function getDataConnector()
+    {
         $connection_name = config('lti.connection_name', config('database.default'));
         return $this->getDataConnectorForConnection($connection_name);
     }
 
     public function getDataConnectorForConnection($connection_name)
     {
-        $pdo = DB::connection()->getPdo();
+        $pdo = DB::connection($connection_name)->getPdo();
         $dbTableNamePrefix = config('database.connections.' . $connection_name . '.prefix', '');
         return DataConnector::getDataConnector($pdo, $dbTableNamePrefix, 'pdo');
     }
